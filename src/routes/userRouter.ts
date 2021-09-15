e11865a3ea881753ddf3849423cba4d366a83864
Tiqur <Tiqur7@gmail.com>
2021-09-15 03:39:00 -0700

    Added basic inline user creation schema

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -3,8 +3,23 @@ import { body } from 'express-validator';
 import { validateRequest } from '../middleware';
 const router = express.Router();
 
-router.get('/', 
-  body('username').isString(),
+
+// Create user endpoint
+router.post('/', 
+  body('email')
+    .isEmail()
+      .withMessage('must be a valid email'),
+
+  body('username')
+    .isLength({ min: 5, max: 16 })
+      .withMessage('must be between 5 and 16 characters long'),
+
+  body('password')
+    .isLength({ min: 5, max: 128 })
+      .withMessage('must be between 5 and 128 characters long')
+    .matches(/\d/)
+      .withMessage('must contain at least 1 number'),
+
   validateRequest,
   (_: Request, res: Response) => {
     res.send(200);
