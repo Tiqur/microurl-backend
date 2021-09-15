e62483e1bb9335ab7aacf9296b991096b67c37eb
Tiqur <Tiqur7@gmail.com>
2021-09-15 03:29:29 -0700

    Added request validation middleware

---
+++ src/middleware/validateRequest.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+
+// Display errors if invalid request parameters
+export function validateRequest(req: Request, res: Response, next: NextFunction) {
+  const errors = validationResult(req);
+
+  res.status(400).json({ errors: errors.array() });
+  next();
+}
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middleware';
 const router = express.Router();
 
-router.get('/', (_: Request, res: Response) => {
-  res.send(200);
+router.get('/', 
+  body('username').isString(),
+  validateRequest,
+  (_: Request, res: Response) => {
+    res.send(200);
 })
 
 export default router;
